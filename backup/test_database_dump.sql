ece74a8642abcc6c39fd01789e6c6b931a524556
Sergey <sergey.sitkarev@gmail.com>
2023-10-24 20:36:24 +0300

    1

---
+++ backup/test_database_dump.sql
@@ -0,0 +1,403 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+--
+-- Roles
+--
+
+CREATE ROLE admin;
+ALTER ROLE admin WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS PASSWORD 'md580a19f669b02edfbc208a5386ab5036b';
+CREATE ROLE postgres;
+ALTER ROLE postgres WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'md53175bce1d3201d16594cebf9d7eb3f9d';
+
+
+
+
+
+
+--
+-- Databases
+--
+
+--
+-- Database "template1" dump
+--
+
+\connect template1
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.12 (Debian 13.12-1.pgdg120+1)
+-- Dumped by pg_dump version 13.12 (Debian 13.12-1.pgdg120+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- Database "admin" dump
+--
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.12 (Debian 13.12-1.pgdg120+1)
+-- Dumped by pg_dump version 13.12 (Debian 13.12-1.pgdg120+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: admin; Type: DATABASE; Schema: -; Owner: admin
+--
+
+CREATE DATABASE admin WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.utf8';
+
+
+ALTER DATABASE admin OWNER TO admin;
+
+\connect admin
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: orders; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.orders (
+    id integer NOT NULL,
+    title character varying(80) NOT NULL,
+    price integer DEFAULT 0
+);
+
+
+ALTER TABLE public.orders OWNER TO postgres;
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.orders_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.orders_id_seq OWNER TO postgres;
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.orders_id_seq OWNED BY public.orders.id;
+
+
+--
+-- Name: orders id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders ALTER COLUMN id SET DEFAULT nextval('public.orders_id_seq'::regclass);
+
+
+--
+-- Data for Name: orders; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.orders (id, title, price) FROM stdin;
+1	War and peace	100
+2	My little database	500
+3	Adventure psql time	300
+4	Server gravity falls	300
+5	Log gossips	123
+6	WAL never lies	900
+7	Me and my bash-pet	499
+8	Dbiezdmin	501
+\.
+
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.orders_id_seq', 8, true);
+
+
+--
+-- Name: orders orders_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- Database "postgres" dump
+--
+
+\connect postgres
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.12 (Debian 13.12-1.pgdg120+1)
+-- Dumped by pg_dump version 13.12 (Debian 13.12-1.pgdg120+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- Database "test_database" dump
+--
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.12 (Debian 13.12-1.pgdg120+1)
+-- Dumped by pg_dump version 13.12 (Debian 13.12-1.pgdg120+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: test_database; Type: DATABASE; Schema: -; Owner: admin
+--
+
+CREATE DATABASE test_database WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.utf8';
+
+
+ALTER DATABASE test_database OWNER TO admin;
+
+\connect test_database
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+--
+-- Name: orders; Type: TABLE; Schema: public; Owner: admin
+--
+
+CREATE TABLE public.orders (
+    id integer,
+    title character varying(80),
+    price integer
+)
+PARTITION BY RANGE (price);
+
+
+ALTER TABLE public.orders OWNER TO admin;
+
+SET default_table_access_method = heap;
+
+--
+-- Name: orders_old; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.orders_old (
+    id integer NOT NULL,
+    title character varying(80) NOT NULL,
+    price integer DEFAULT 0
+);
+
+
+ALTER TABLE public.orders_old OWNER TO postgres;
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.orders_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.orders_id_seq OWNER TO postgres;
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.orders_id_seq OWNED BY public.orders_old.id;
+
+
+--
+-- Name: orders_less499; Type: TABLE; Schema: public; Owner: admin
+--
+
+CREATE TABLE public.orders_less499 (
+    id integer,
+    title character varying(80),
+    price integer
+);
+ALTER TABLE ONLY public.orders ATTACH PARTITION public.orders_less499 FOR VALUES FROM (0) TO (499);
+
+
+ALTER TABLE public.orders_less499 OWNER TO admin;
+
+--
+-- Name: orders_more499; Type: TABLE; Schema: public; Owner: admin
+--
+
+CREATE TABLE public.orders_more499 (
+    id integer,
+    title character varying(80),
+    price integer
+);
+ALTER TABLE ONLY public.orders ATTACH PARTITION public.orders_more499 FOR VALUES FROM (499) TO (2147483647);
+
+
+ALTER TABLE public.orders_more499 OWNER TO admin;
+
+--
+-- Name: orders_old id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders_old ALTER COLUMN id SET DEFAULT nextval('public.orders_id_seq'::regclass);
+
+
+--
+-- Data for Name: orders_less499; Type: TABLE DATA; Schema: public; Owner: admin
+--
+
+COPY public.orders_less499 (id, title, price) FROM stdin;
+1	War and peace	100
+3	Adventure psql time	300
+4	Server gravity falls	300
+5	Log gossips	123
+\.
+
+
+--
+-- Data for Name: orders_more499; Type: TABLE DATA; Schema: public; Owner: admin
+--
+
+COPY public.orders_more499 (id, title, price) FROM stdin;
+2	My little database	500
+6	WAL never lies	900
+7	Me and my bash-pet	499
+8	Dbiezdmin	501
+\.
+
+
+--
+-- Data for Name: orders_old; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.orders_old (id, title, price) FROM stdin;
+1	War and peace	100
+2	My little database	500
+3	Adventure psql time	300
+4	Server gravity falls	300
+5	Log gossips	123
+6	WAL never lies	900
+7	Me and my bash-pet	499
+8	Dbiezdmin	501
+\.
+
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.orders_id_seq', 8, true);
+
+
+--
+-- Name: orders_old orders_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders_old
+    ADD CONSTRAINT orders_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- PostgreSQL database cluster dump complete
+--
+
